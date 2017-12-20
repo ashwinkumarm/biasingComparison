b34f4f6864216f00adbd0bbf65dda453ed065de6
ashwinkumarm <ashbushster@gmail.com>
2017-12-19 23:25:53 -0600

    Comparison of 3 biasing techniques - KMM, ulSif and kliep

---
+++ bigDataProj/constants/constants.py
@@ -0,0 +1,20 @@
+'''
+Created on 19-Dec-2017
+
+@author: Ashwin
+'''
+# file paths constant
+# trainingSet = "Atrocity"
+# trainDataPath = "D:\Downloads\Geolocation\Atrocityemb.txt"
+# trainlabelPath = "D:\Downloads\Geolocation\AtrocityLabel.txt"
+# testSet = "NYTAtrocity"
+# testDataPath = "D:\Downloads\Geolocation\NYTAtrocity1000emb.txt"
+# testlabelPath = "D:\Downloads\Geolocation\NYTAtrocity1000Label.txt"
+
+
+trainingSet = "NYTAtrocity"
+trainDataPath = "D:\Downloads\Geolocation\NYTAtrocity1000emb.txt"
+trainlabelPath = "D:\Downloads\Geolocation\NYTAtrocity1000Label.txt"
+testSet = "Atrocity"
+testDataPath = "D:\Downloads\Geolocation\Atrocityemb.txt"
+testlabelPath = "D:\Downloads\Geolocation\AtrocityLabel.txt"
